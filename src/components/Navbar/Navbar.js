517055f02a39fa3b2942fc31aba677df7d0fbf80
Cris <cristina.picatoste@gmail.com>
2021-04-02 17:49:27 +0200

    Nav added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/LOGO_CRIS.svg';
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Link to="/" className="navbar-logo-container">
+        <Logo className="navbar-logo" />
+      </Link>
+      <div className="navbar-options">
+        <Link to="/portfolio" className="navbar-option">PORTFOLIO</Link>
+        <Link to="/about" className="navbar-option">ABOUT ME</Link>
+        <Link to="/contact" className="navbar-option">CONTACT</Link>
+        {/* {
+          currentUser
+          ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+          : <Link to="/signin" className="option">SIGN IN</Link>
+        } */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
