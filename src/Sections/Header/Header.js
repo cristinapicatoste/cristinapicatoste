03db9b025124ab540558b387f45ad67074b7445f
Cris <cristina.picatoste@gmail.com>
2021-04-03 01:04:39 +0200

    text header added, need to be updated

--- src/Sections/Header/Header.js
+++ src/Sections/Header/Header.js
@@ -4,7 +4,9 @@ import './Header.scss';
 const Header = () => {
   return (
     <div className='header'>
-      
+      <h3>Cristina Picatoste</h3>
+      <h1>Full Stack Developer</h1>
+      <h4>MERN Developer</h4>
     </div>
   )
 }
