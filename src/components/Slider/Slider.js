127eab807d4e0e5c963eb635d4391b8e644b74b8
Cris <cristina.picatoste@gmail.com>
2021-04-08 11:16:35 +0200

    data created and fadeIn animations added

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,7 +5,7 @@ export const Slider = () => {
   return (
     <>
       <div className="header">        
-        <p className="fadeInRight greyMedium">Cristina Picatoste</p>
+        <p className="fadeInRight greyMedium size1">Cristina Picatoste</p>
         <h1 className="fadeInLeft size7 grey">MERN</h1>
         <h2 className="fadeInLeft size7 greyLight">Stack Developer</h2>
         <p className="fadeInUp greyMedium">MongoDB, Express, React & Node</p>
---
+++ src/data/projects.js
@@ -0,0 +1,48 @@
+export const projects = [
+ {
+   "id": 001,
+   "project": "Debona Jewels",
+   "subtitle": "E-commerce",
+   "description": "E-commerce developed with shopify and woocomerce. Currently being rebuild in React. Available soon!",
+   "year": "2017, 2019, 2021 (last update)",
+   "tech": "Shopify & Wordpress",
+   "image": "assets/portfolio/debona.png"
+ },
+ {
+   "id": 002,
+   "project": "Sound Mist",
+   "subtitle": "Music everywhere",
+   "description": "Music platform to upload and listen your favourite songs and artists!",
+   "year": "2021",
+   "tech": "React, Node, MongoDB, Express, Mongoose & Atlas",
+   "image": "assets/portfolio/soundmist.png"
+ },
+ {
+   "id": 003,
+   "project": "Iris Therapy",
+   "subtitle": "Grow, learn & evolve",
+   "description": "A site to grow and develop. Therapies, trainings and events",
+   "year": "2018",
+   "tech": "React, Node, MongoDB, Express, Mongoose & Atlas",
+   "image": "assets/portfolio/iristherapy.png"
+ },
+ {
+   "id": 004,
+   "project": "TinDog",
+   "subtitle": "Pet Lovers",
+   "description": "A site to grow and develop. Therapies, trainings and events",
+   "year": "2019",
+   "tech": "React, Node, MongoDB, Express, Mongoose & Atlas",
+   "image": "assets/portfolio/tindog.png"
+ },
+ {
+   "id": 005,
+   "project": "Project 22",
+   "subtitle": "Numerology Calculator",
+   "description": "A site to grow and develop. Therapies, trainings and events",
+   "year": "2020",
+   "tech": "React, Node, MongoDB, Express, Mongoose & Atlas",
+   "image": "assets/portfolio/project22.png"
+ },
+
+]
