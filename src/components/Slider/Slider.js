edbe45a52a300c818cd9fe7b8002e42ed3638358
Cris <cristina.picatoste@gmail.com>
2021-04-09 19:47:00 +0200

    Estilos y lógica transición slider actualizada y funcionando correctamente

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,46 +4,33 @@ import { skills } from '../../data/skills';
 import './Slider.scss';
 
 export const Slider = () => {
-  const [fadeOut, setFadeOut] = useState(false);
   const [skill, setSkill] = useState(0);
 
   useEffect(() => {
-    // skills.map((s, i) => {      
-    //   setTimeout(() => {
-    //     setFadeOut(!fadeOut);
-    //     // setSkill(i);
-    //   }, 3000);
-    // })
-
-    setInterval(() => {
-      setFadeOut(!fadeOut);
-      setFadeOut(!fadeOut);
-      setTimeout(() => {
-        setSkill(1)
-      }, 2000);
-    }, 3000);
-  }, [skill, fadeOut])
-
+    setTimeout(() => {
+      if (skill === 0) {
+        setSkill(1);
+      } if (skill === 1) {
+        setSkill(2);
+      } if (skill === 2) {
+        setSkill(0)
+      }
+    }, 5000);
+  }, [skill])
 
   return (
     <>
       <div className="header">        
-        {/* <p className="fadeInRight greyMedium experience">{skills[skill].experience}</p>  */}
-        <p className={`${fadeOut ? 'fadeOutRight' : 'fadeInRight'} greyMedium experience`}>{skills[skill].experience}</p> 
-        {/* <h1 className="fadeInLeft size7 grey">{skills[1].position1}</h1>  */}
-        <h1 className={`${fadeOut ? 'fadeOutLeft' : 'fadeInLeft'} size7 grey`}>{skills[skill].position1}</h1> 
-        {/* <h2 className="fadeInLeft size7 greyLight">{skills[skill].position2}</h2>  */}
-        <h2 className={`${fadeOut ? 'fadeOutLeft' : 'fadeInLeft'} size7 greyLight`}>{skills[skill].position2}</h2> 
-        {/* <p className="fadeInUp greyMedium technologies">{skills[skill].technologies}</p>  */}
-        <p className={`${fadeOut ? 'fadeOutUp' : 'fadeInUp'} greyMedium technologies`}>{skills[skill].technologies}</p> 
+        <p className="fadeInOutDown greyMedium experience">{skills[skill].experience}</p>  
+        <h1 className="fadeInOutLeft size7 grey">{skills[skill].position1}</h1>
+        <h2 className="fadeInOutLeft size7 greyLight">{skills[skill].position2}</h2> 
+        <p className="fadeInOutUp greyMedium technologies">{skills[skill].technologies}</p> 
       </div>
       <div className="square">
-        {/* <div className="square-image fadeInRight"> */}
-        <div className={`${fadeOut ? 'fadeOutRight' : 'fadeInRight'} square-image`}>
-        </div>
-        {/* <h1 className="square-text greyLight size7 fadeInUpRotated">{skills[skill].calltoaction}</h1>  */}
-        <h1 className={`${fadeOut ? 'fadeOutDownRotated' : 'fadeInUpRotated'} square-text greyLight size7`}>{skills[skill].calltoaction}</h1> 
+        <div className="square-image fadeInOutRight" style={{backgroundImage: `url(${skills[skill].image})`}}></div>
+        <h1 className="square-text greyLight size7 fadeInOutUpRotated">{skills[skill].calltoaction}</h1> 
       </div>
     </>
   )
 }
+
